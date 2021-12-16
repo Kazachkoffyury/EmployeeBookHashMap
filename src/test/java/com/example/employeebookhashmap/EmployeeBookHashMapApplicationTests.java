56c123df8993e76bfc736d1a040ca1ce2f91e047
Kazackoffyury <kazachkoff.yury@yandex.ru>
2021-12-16 15:09:06 +0300

    initialize

---
+++ src/test/java/com/example/employeebookhashmap/EmployeeBookHashMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeebookhashmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeBookHashMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
